8d8f882cbcdf75f3cd545ae7c3761085943ac15e
Jeffrey Sadeli <jsadeli@users.noreply.github.com>
2025-04-11 17:48:53 +0700

    feat: switch from powerline to meslo nerd font

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,19 +4,19 @@ OFF="\033[0m"           # text reset
 YELLOW="\033[0;33m"
 
 # ------------------------------------------------------------------------------
-# zsh configuration
+# zsh configurations
 # ------------------------------------------------------------------------------
 
-echo -e "${YELLOW}-> Install powerline fonts...${OFF}"
-git clone --depth=1 https://github.com/powerline/fonts.git
-cd fonts || exit
-./install.sh
-cd .. && rm -rf fonts
+echo -e "${YELLOW}-> Install 'Meslo LGM NerdFont Mono Regular'...${OFF}"
+mkdir -p ~/.local/share/fonts
+(cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/patched-fonts/Meslo/M/Regular/MesloLGMNerdFontMono-Regular.ttf)
 
-echo -e "${YELLOW}-> Install Oh My Posh and custom theme...${OFF}"
+echo -e "${YELLOW}-> Install 'Oh My Posh' and custom theme...${OFF}"
 curl -s https://ohmyposh.dev/install.sh | bash -s
 mkdir -p "$HOME/.oh-my-posh"
 curl "https://raw.githubusercontent.com/jsadeli/dotfiles/refs/heads/main/configs/p10k_lean.omp.json" -o "$HOME/.oh-my-posh/p10k_lean.omp.json"
 
 echo -e "${YELLOW}-> Copy customized zsh config...${OFF}"
 cp .devcontainer/configs/.zshrc "$HOME/.zshrc"
+
+echo -e "${YELLOW}-> Done!${OFF}"
